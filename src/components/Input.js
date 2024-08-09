2f15dd72362c9247c93ad25e82db0326a5d4edad
valeriia <valeriiadesign14@gmail.com>
2024-08-09 13:02:18 +0900

    project created

---
+++ src/components/Input.js
@@ -0,0 +1,26 @@
+import React from "react";
+const Input = ({ value, label = "A", onChange, className }) => {
+  return (
+    <div className={" input-container col-3 p-0" + className}>
+      <div className="border border-dark d-flex">
+        <div className="d-flex justify-content-center align-content-center">
+          <input
+            className="control-label  border-0"
+            type="text"
+            value={label}
+          />
+        </div>
+        <div className="flex-grow-1">
+          <input
+            type="text"
+            value={value}
+            className="control-input border-0 w-100 text-center"
+            placeholder="-"
+            onChange={(e) => onChange(e.target.value)}
+          ></input>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Input;
---
+++ src/pages/index.js
@@ -0,0 +1,107 @@
+import React, { useState, useEffect } from "react";
+import Input from "../components/Input";
+import io from 'socket.io-client';
+const socket = io('http://localhost:4000');
+
+const Home = () => {
+  const [data, setData] = useState(
+    [
+      {
+        label: 'A',
+        value: ''
+      },
+      {
+        label: 'E',
+        value: ''
+      },
+      {
+        label: 'I',
+        value: ''
+      },
+      {
+        label: 'M',
+        value: ''
+      },
+      {
+        label: 'B',
+        value: ''
+      },
+      {
+        label: 'F',
+        value: ''
+      },
+      {
+        label: 'J',
+        value: ''
+      },
+      {
+        label: 'N',
+        value: ''
+      },
+      {
+        label: 'C',
+        value: ''
+      },
+      {
+        label: 'G',
+        value: ''
+      },
+      {
+        label: 'K',
+        value: ''
+      },
+      {
+        label: 'O',
+        value: ''
+      },
+      {
+        label: 'D',
+        value: ''
+      },
+      {
+        label: 'H',
+        value: ''
+      },
+      {
+        label: 'L',
+        value: ''
+      },
+      {
+        label: 'P',
+        value: ''
+      }
+    ]
+  );
+  useEffect(() => {
+
+    return() => {
+      socket.off('message');
+    }
+  }, []);
+
+  const changeValue = (v, index, item) => {
+    const regex = /^[a-zA-Z0-9]{1,3}$/;
+    if(!regex.test(v)) return;
+    let _data = data;
+    _data[index] = {
+      label: item.label,
+      value: v
+    };
+    setData([..._data]);
+    socket.emit('message', JSON.stringify(_data));
+  }
+  return (
+    <div className="mt-5">
+      <div className="grid-container">
+      <div className="container">
+      <div className="row">
+        {data.map((item, index) => (
+          <Input  value = {item.value} label={item.label} index = {index} onChange={(v) => changeValue(v, index, item)} className={(index %4==0)?" input-border-right ":" input-border"}/>
+        ))}
+      </div>
+      </div>
+      </div>
+    </div>
+  );
+};
+export default Home;
